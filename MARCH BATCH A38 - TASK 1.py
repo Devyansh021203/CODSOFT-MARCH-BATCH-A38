64ebd87fa55828334e17a92061a698792e442bd3
Devyansh021203 <162180843+Devyansh021203@users.noreply.github.com>
2024-04-01 18:07:42 +0530

    Update MARCH BATCH A38 - TASK 1.py

--- MARCH BATCH A38 - TASK 1.py
+++ MARCH BATCH A38 - TASK 1.py
@@ -1,3 +1,8 @@
+'''It is a simple chatbot that responds to user inputs based on
+predefined rules. I used if-else statements or pattern matching
+techniques to identify user queries and provide appropriate
+responses. This will give you a basic understanding of natural language processing and conversation flow'''
+
 def respond_to_user_input(user_input):
 
     user_input = user_input.lower()
